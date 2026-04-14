341c8c9015208ee1e5c4e8bcb3c7e5bccb5167fb
AOlivares <olivaresadriana2002@gmail.com>
2026-04-14 14:11:17 -0500

    feat: modelo event

---
+++ app/Models/Event.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'date',
+        'time',
+        'location',
+        'status',
+        'capacity',
+        'has_parking',
+        'parking_slots',
+        'user_id',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'date' => 'date',
+            'has_parking' => 'boolean',
+            'capacity' => 'integer',
+            'parking_slots' => 'integer',
+            'user_id' => 'integer',
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
